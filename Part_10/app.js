4033ea841b0653b331756305101c5b6a1a4ff8a4
isrt09 <mazedur@gmail.com>
2021-02-17 01:19:48 +0600

    Loop Directives

---
+++ Part_10/app.js
@@ -0,0 +1,15 @@
+var X = new Vue({
+
+	el : '#app',
+	data:{
+		courses:['SSC','HSC', 'BBA','MBA','PHD'],
+		fruits :[
+			{name:'Apple',country:'Bangladesh',district:'Dinajpur'},
+			{name:'Apple',country:'Bangladesh',district:'Dinajpur'},
+			
+		]
+	},
+	methods:{		
+		
+	}
+})
