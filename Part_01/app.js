38e0fb9f1192b38c761587fae0c64a81c259ee25
isrt09 <mazedur@gmail.com>
2021-02-16 13:13:24 +0600

    Support, Instance, Element

---
+++ Part_01/app.js
@@ -0,0 +1,13 @@
+var x = new Vue({
+	el   : '#app',
+	data :{
+		data : 'Welcome to Vue JS',
+		message: 'Mazedur'
+	}			
+});
+var y = new Vue({
+	el   : '#component',
+	data : {
+		message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sunt magni quam ullam, culpa deserunt dolorum quibusdam quaerat ab? Fuga.'
+	}
+});
