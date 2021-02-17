dcacfc07bc46f1e93e26f4651f3aa06746e65d17
isrt09 <mazedur@gmail.com>
2021-02-18 01:25:04 +0600

    Props & Scoped

---
+++ Part-12/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+     <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <blockquote class="blockquote context">
+          <p class="mb-0">Part 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+          <footer class="blockquote-footer">Someone famous in
+            <cite title="Source Title">Source Title</cite>
+          </footer>
+        </blockquote>
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+</template>
