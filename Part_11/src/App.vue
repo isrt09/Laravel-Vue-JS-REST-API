512dc4f921404162d8e78a9c1fb7e79474d57491
isrt09 <mazedur@gmail.com>
2021-02-17 22:09:10 +0600

    Vue CLI Components

---
+++ Part_11/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app"> 
+    <Navigation></Navigation>
+    <Content></Content> 
+    <Footer></Footer>   
+  </div>
+</template>
+
+<script>
+import Navigation  from './components/Navigation.vue'
+import Content  from './components/Content.vue'
+import Footer  from './components/Footer.vue'
+export default {          
+    name:'app',
+    components:{
+      Navigation,
+      Content,         
+      Footer
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.container{
+  text-align: left;
+}
+</style>
