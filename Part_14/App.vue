b45a28dad398fd087f7971ae152df892d2ba3fc6
isrt09 <mazedur@gmail.com>
2021-02-18 11:06:01 +0600

    Axios Request

---
+++ Part_14/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app"> 
+    <Hook></Hook>
+  </div>
+</template>
+
+<script>
+import Hook from './components/Hook.vue'
+export default {          
+    name:'app',
+    data(){
+      return{
+        
+      }
+    },
+    components:{
+      Hook
+    }   
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Part_14/components/Hook.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="hook">
+                <h1>Post</h1>
+                <hr>
+                <div v-for="post in posts" :key="post.id">
+                    <h3>{{post.title}}</h3>
+                    <p>{{post.body}}</p>
+                </div>                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'hook',
+    data(){
+        return{
+            posts : []
+        }
+    },
+    methods:{
+
+    },
+    created(){
+            axios.get('https://jsonplaceholder.typicode.com/posts/')
+            .then(response=>{
+                this.posts = response.data
+            })
+            .catch(error=>{
+                console.log(error)
+            })
+        }
+}
+</script>
+
+<style scoped>
+h1{
+    color:red
+}
+.hook{
+    text-align: justify;
+}
+</style>
